2d351bd6ddcb3adb11e163168731b8ffe3b6988b
ximet <dq.ximet@gmail.com>
2017-05-22 18:26:47 +0300

    check support

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 'use strict';
 module.exports = input => {
-	const el = document.createElement('textarea');
-
-	el.value = input;
-
-	// Prevent keyboard from showing on mobile
-	el.setAttribute('readonly', '');
-
-	el.style.contain = 'strict';
-	el.style.all = 'unset';
-	el.style.position = 'absolute';
-	el.style.left = '-9999px';
-	el.style.fontSize = '12pt'; // Prevent zooming on iOS
-
-	document.body.appendChild(el);
-	el.select();
-
 	let success = false;
-	try {
-		success = document.execCommand('copy');
-	} catch (err) {}
 
-	document.body.removeChild(el);
+  //check for support (not support in Andoid Browser, in Safari 9, in mobile Safari 8, etc...)
+	if (document.execCommand) {
+		const el = document.createElement('textarea');
+
+		el.value = input;
+
+		// Prevent keyboard from showing on mobile
+		el.setAttribute('readonly', '');
+
+		el.style.contain = 'strict';
+		el.style.all = 'unset';
+		el.style.position = 'absolute';
+		el.style.left = '-9999px';
+		el.style.fontSize = '12pt'; // Prevent zooming on iOS
+
+		document.body.appendChild(el);
+		el.select();
+
+		try {
+			success = document.execCommand('copy');
+		} catch (err) {}
+
+		document.body.removeChild(el);
+	}
 
 	return success;
 };
