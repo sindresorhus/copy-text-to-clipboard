8ea169dd93b9ede3cbb8e93e12cf3b992e605f8f
neal1991 <neal1991@sina.com>
2017-06-09 09:36:31 +0800

    fix for issue #7

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ module.exports = input => {
 	el.setAttribute('readonly', '');
 
 	el.style.contain = 'strict';
-	el.style.all = 'unset';
 	el.style.position = 'absolute';
 	el.style.left = '-9999px';
 	el.style.fontSize = '12pt'; // Prevent zooming on iOS
 
 	document.body.appendChild(el);
-	select(el);
+	el.select();
 
 	let success = false;
 	try {
