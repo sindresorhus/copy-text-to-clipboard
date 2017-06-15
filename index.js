0571a2f3a887b327b663b1bd2531ebafefffa2f3
Jonas Gierer <jgierer12@outlook.com>
2017-06-15 21:18:30 +0200

    Restore previous selection after copying (#11)

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ module.exports = input => {
 	el.style.left = '-9999px';
 	el.style.fontSize = '12pt'; // Prevent zooming on iOS
 
+	const selection = getSelection();
+	let originalRange = false;
+	if (selection.rangeCount > 0) {
+		originalRange = selection.getRangeAt(0);
+	}
+
 	document.body.appendChild(el);
 	el.select();
 
@@ -21,6 +27,11 @@ module.exports = input => {
 	} catch (err) {}
 
 	document.body.removeChild(el);
+
+	if (originalRange) {
+		selection.removeAllRanges();
+		selection.addRange(originalRange);
+	}
 
 	return success;
 };
